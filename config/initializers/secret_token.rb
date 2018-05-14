2864e6ef1c37a52fb508fdcb7f8b8f17ffaef947
SidSoft <sidsoftlab@gmail.com>
2018-05-14 11:58:09 +0300

    Init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Vsol::Application.config.secret_key_base = '8d7c638467cdae83681a8c887f970d6e3a960ada6131a16e73f49196a24f8aee20c94beb9f17ef441cfc0e6a7888b9d5f7aae2897f14762182c310475ce64d9c'
